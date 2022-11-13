93080ccaac1661f976fd7c6780ebfa4e32f3b91f
Arvel_Cavitt <105776427+ArvelCavitt@users.noreply.github.com>
2022-11-12 18:16:08 -0800

    commit on 'main'

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from flask_app import app
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
---
+++ flask_app/models/user.py
@@ -0,0 +1,36 @@
+from flask_app import app
+from flask_app.config.mysqlconnectin import connectToMySQL
+from flask import flash
+from flask_bcrypt import Bcrypt
+# from flask_app.models import "_______"
+import re
+
+bcrypt = Bcrypt(app)
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class User:
+    db = "user_properties_schema"
+    self.id = data['id']
+    self.first_name = data['first_name']
+    self.last_name = data['last_name']
+    self.email = data['email']
+    self.password = data['password']
+    self.salary = data['salary']
+    self.created_at = data['created_at']
+    self.updated_at = data['updated_at']
+    # self."_______" = []
+
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO users (first_name, last_name, email, password, salary) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s, %(salary)s)"
+        return connectToMySQL(cls.db).query_db(query,data)
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(cls.db).query_db(query)
+        users = []
+        for row in results:
+            users.append(cls(row))
+        return users
