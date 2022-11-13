91be55fcb86a3b2b0b6e29485356ca4bd92d652c
NoorMusheer <NoorMusheer@aol.com>
2022-11-13 11:17:53 -0800

    now able to get address and price info from results

--- flask_app/properties_api.py
+++ flask_app/properties_api.py
@@ -1,4 +1,5 @@
 import http.client
+import json
 
 conn = http.client.HTTPSConnection("realty-in-us.p.rapidapi.com")
 
@@ -7,11 +8,18 @@ headers = {
     'X-RapidAPI-Host': "realty-in-us.p.rapidapi.com"
     }
 
-conn.request("GET", "/properties/list-for-sale?state_code=NY&city=New%20York%20City&offset=0&limit=10&sort=relevance", headers=headers)
+conn.request("GET", "/properties/list-for-sale?state_code=CA&city=Fremont&offset=0&limit=3&sort=relevance&price_max=500000", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
 
-# print(data.decode("utf-8"))
+# print(data.decode("utf-8")['meta'])
 
-print(data)
+parse_json = json.loads(data)
+
+prop_data = parse_json['listings']
+for each in prop_data:
+    print(each['address'])
+    print(each['price'])
+
+
