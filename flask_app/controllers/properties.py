576b5e4412c8d7b8970cdcb3ca6efccf473cc0aa
Jedherrera <104804622+Jedherrera@users.noreply.github.com>
2022-11-16 00:08:03 -0800

    commit to main

--- flask_app/controllers/properties.py
+++ flask_app/controllers/properties.py
@@ -3,25 +3,37 @@ from flask_app.models import user, property
 from flask_app.controllers.mtg_rates import avg_rate
 from flask import render_template, redirect, session, request
 
-@app.route('/params', methods = ['POST'])
-def user_parameters ():
+
+@app.route('/affordablehomes/home')
+def user_home():
+    return render_template('dashboard_homes.html')
+
+
+@app.route('/affordablehomes/condo')
+def user_condo():
+    return render_template('dashboard_condo.html')
+
+
+@app.route('/affordablehomes/estimate')
+def estimate_user():
+    return render_template('estimate_page.html')
+
+
+@app.route('/affordablehomes/estimate/results', methods=['POST'])
+def user_parameters():
     mtg_data_input = {
-        "credit_score":request.form['credit_score'],
-        "down_payment":request.form['down_payment'],
+        "credit_score": request.form['credit_score'],
+        "down_payment": request.form['down_payment'],
         "P": request.form['max_monthly'],
     }
     max_price = user.User.get_max_price(mtg_data_input)
     frmtd_city = (request.form['city']).replace(" ", "_")
 
     home_data_input = {
-        "city":frmtd_city,
-        "state":request.form['state'],
-        "radius":request.form['radius'],
-        "max_price" : max_price
+        "city": frmtd_city,
+        "state": request.form['state'],
+        "radius": request.form['radius'],
+        "max_price": max_price
     }
     addresses = property.Property.get_listings_by_max_price(home_data_input)
-    return render_template('results.html', addresses = addresses)
-
-@app.route ('/dashboard_practice')
-def user_dashboard():
-    return render_template('practice_user_input.html')
+    return render_template('results.html', addresses=addresses)
